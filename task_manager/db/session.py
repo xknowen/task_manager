6fc9f213a2545856a764ad73cefe69006a3a6280
24072 <alex240720008@gmail.com>
2025-08-26 14:39:27 +0700

    Базовый класс бд вынесен в отдельный пакет

--- task_manager/db/session.py
+++ task_manager/db/session.py
@@ -1,13 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
 
-from .config import settings
-
-
-class Base(DeclarativeBase):
-    pass
+from task_manager.core.config import get_settings
 
 
+settings = get_settings()
 engine = create_async_engine(settings.database_url, echo=False, future=True)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
