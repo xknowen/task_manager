7cf9750b7c6c880e4935c1368c9147e0949a4ecf
24072 <alex240720008@gmail.com>
2025-08-26 14:41:46 +0700

    Класс TaskStatus вынесен в отдельный пакет + изменён тип столбца с str на uuid

--- task_manager/db/models/task.py
+++ task_manager/db/models/task.py
@@ -1,25 +1,22 @@
 from __future__ import annotations
-from enum import Enum
-from uuid import uuid4
+
+import uuid
+from uuid import UUID
 from typing import Optional
 
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID as sqUUID
 from sqlalchemy import String, Enum as SAEnum, Text
 
-from .db import Base
-
-
-class TaskStatus(str, Enum):
-    CREATED = "created"
-    IN_PROGRESS = "in_progress"
-    COMPLETED = "completed"
+from .task_status import TaskStatus
+from task_manager.db.base import Base
 
 
 class Task(Base):
     __tablename__ = "tasks"
 
-    id: Mapped[str] = mapped_column(
-        String(36), primary_key=True, default=lambda: str(uuid4())
+    id: Mapped[UUID] = mapped_column(
+        sqUUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
     title: Mapped[str] = mapped_column(String(200), nullable=False, index=True)
     description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
