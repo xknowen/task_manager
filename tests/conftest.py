4864c37012c3c44f7326ccbf152751d313fe2ef3
24072 <alex240720008@gmail.com>
2025-08-26 14:53:08 +0700

    Исправлены импорты

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from task_manager.db import Base, get_session
+from task_manager.db.base import Base
+from task_manager.db.dependencies import get_db_session
 from task_manager.main import app
 
 
@@ -38,7 +39,7 @@ async def client(session):
     async def override_get_session():
         yield session
 
-    app.dependency_overrides[get_session] = override_get_session
+    app.dependency_overrides[get_db_session] = override_get_session
 
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
